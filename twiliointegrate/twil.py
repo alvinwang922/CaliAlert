db349c64ffc1d9659501f8630b8374011271b6d2
HenrySusantin <hsusanti@uci.edu>
2020-02-02 00:46:22 -0800

    hopefully this works

--- twiliointegrate/twil.py
+++ twiliointegrate/twil.py
@@ -1,6 +1,14 @@
 from twilio.rest import Client
 from collections import defaultdict
+
+
+from firebase_admin import credentials
+from firebase_admin import firestore
 import json
+
+cred = credentials.Certificate("calialert-firebase-adminsdk-2wgqz-70c2fa06a9.json")
+    
+db = firestore.client()
 
 class Twilio():
     account_sid = 'AC256d2fc18b15ba880d3222d578211f49'
@@ -9,25 +17,26 @@ class Twilio():
     call_dict = defaultdict(list)
     
     def __init__(self):
-        self.read_json()
+        self.from_firestore_populate()
         self.zip_store()
         self.client = Client(self.account_sid, self.auth_token)
         
     def zip_store(self):
         with open('states.json', 'r') as json_file:
             self.state_zip = json.load(json_file)
-        
-    def json_populate(self):
-        for num in self.client.messages.list(to=self.phone_num):
-            if num.from_ not in self.call_dict[num.from_[2:5]]:
-                self.call_dict[num.from_[2:5]].append(num.from_)
-        with open('numbers.json', 'w') as outfile:
-            json.dump(self.call_dict, outfile)
-    
-    def read_json(self):
-        with open('numbers.json', 'r') as json_file:
-            self.call_dict = json.load(json_file)
+            
+    def city_locate(self, zip_code):
+        for key in self.state_zip.keys():
+            for city in self.state_zip[key]:
+                for zips in city:
+                    if zips == zip_code:
+                        return (city, zips)
 
+            
+    def from_firestore_populate(self):
+        docs = db.collection(u'users').stream()
+        for doc in docs:
+            self.call_dict[doc.to_dict()['zip']].append(doc.to_dict()['phoneNumber'])
         
     def numbers_to_call(self, area_code: str or list(str)) -> list:
         return_list = []
@@ -52,5 +61,13 @@ class Twilio():
 if __name__ == '__main__':
     t = Twilio()
     print(t.call_dict)
-    t.sms_func(t.numbers_to_call('510'), 'h i  b u r w i n im pha si n g in  n o  u t of e x ist en ce')
+    t.sms_func(t.numbers_to_call('94555'), 'h i  b u r w i n im pha si n g in  n o  u t of e x ist en ce')
+    # Import database module.
     
+    
+    # db = firebase.database()
+    # print(db.child('/users').get().val()
+          
+    # Import database module.
+
+        
